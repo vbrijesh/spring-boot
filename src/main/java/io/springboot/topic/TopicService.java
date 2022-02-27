6cc605bbd9311b93aaae629ab4a718c114e258f4
Brijesh verma <41379406+vbrijesh@users.noreply.github.com>
2022-02-27 18:07:13 +0530

    Spring Boot Initial static changes

---
+++ src/main/java/io/springboot/topic/TopicService.java
@@ -0,0 +1,45 @@
+package io.springboot.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring","spring framework", "spring description"),
+			new Topic("java","Core java", "Core java description"),
+			new Topic("javascripts","javascript desc", "Javascript desc")
+			));
+	
+	public List<Topic> getAllTopic() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for(int i=0; i<topics.size();i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+
+}
